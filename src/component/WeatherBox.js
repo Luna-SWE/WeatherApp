9bd811e5d86fccb48c03ae89556a4bb91778d4fa
Luna <hyeyoung.luna.lee@gmail.com>
2024-08-18 21:44:13 +0800

    🐛 Fix: Truncate the decimal number to one decimal places

--- src/component/WeatherBox.js
+++ src/component/WeatherBox.js
@@ -6,7 +6,10 @@ const WeatherBox = ({ weather }) => {
   return (
     <div className='weather-box'>
       <div>{weather?.name}</div>
-      <h2>{`${weather?.main.temp}°C | ${weather?.main.temp * 1.8 + 32}F`}</h2>
+      <h2>{`${weather?.main.temp.toFixed(1)}°C | ${(
+        weather?.main.temp * 1.8 +
+        32
+      ).toFixed(1)}F`}</h2>
       <h3>{weather?.weather[0].description}</h3>
     </div>
   );
